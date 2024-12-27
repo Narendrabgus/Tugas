a7d4026a9443009ea7a9c87c5825e09cd14805ee
Narendrabgus <111202314983@mhs.dinus.ac.id>
2024-12-27 08:16:39 +0700

    first commit

---
+++ ajax/process5.php
@@ -0,0 +1,16 @@
+<?php 
+$myarry = array(
+    "teks 1", "teks 2", "teks 3", "teks 4", "teks 5", "teks 6", "teks 7"
+);
+
+$randomize1 = array_rand($myarry);
+$randomize2 = array_rand($myarry);
+$randomize3 = array_rand($myarry);
+
+$myvals = array(
+    'satu' => '1' . $myarry[$randomize1],
+    'dua' => '2' . $myarry[$randomize2],
+    'tiga' => '3' . $myarry[$randomize3]
+);
+
+echo json_encode($myvals);
